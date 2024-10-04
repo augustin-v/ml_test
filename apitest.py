b25cc346c695e07c9e780be1a9e1c7ad7fb38835
Augustin Villetard <avilletardpiano@gmail.com>
2024-10-05 02:53:26 +0900

    fix: dependencies

--- apitest.py
+++ apitest.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 import joblib
 import xgboost as xgb
 import numpy as np
 
 app = Flask(__name__)
+
+# Enable CORS for your Flask app
+CORS(app, resources={r"/predict": {"origins": "http://localhost:3000"}})
 
 # Load the trained model
 model = xgb.XGBRegressor()
